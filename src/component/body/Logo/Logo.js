7e3e73304834c90aef83e8833455b7d1093d8301
Rafael Amaral <rafaelbmo@live.com>
2023-03-04 19:55:32 -0300

    up

--- src/component/body/Logo/Logo.js
+++ src/component/body/Logo/Logo.js
@@ -2,12 +2,13 @@ import React from 'react'
 import styles from './h1Logo.module.css'
 import Image from 'next/image'
 import fictopialogo from "./fictopia-logo.svg"
+import Link from 'next/link'
 
 
 const Logo = () => {
   return (
     <div className={styles.logoContainer}>
-      <Image className={styles.LogoImage} src={fictopialogo} alt="a"/>
+      <Image className={styles.LogoImage} src={fictopialogo} alt="a" />
     </div>
   )
 }
--- src/component/header/menu/Menu.js
+++ src/component/header/menu/Menu.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
-import styles from './menu.module.css'
+import Link from 'next/link';
+import React, { useState } from 'react';
+import styles from './menu.module.css';
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -8,26 +9,29 @@ const Header = () => {
     setIsOpen(!isOpen);
   };
 
+  const handleLinkClick = () => {
+    setIsOpen(false);
+  };
+
   return (
     <header className={`${styles.header}`}>
       <nav className={`${styles.nav}`}>
-        <div className={`${styles.logo}`}>
-          <a href="/">Logo</a>
-        </div>
+
         <button className={`${styles.toggle}`} onClick={handleToggle}>
           <span className={`${styles.bar}`}></span>
           <span className={`${styles.bar}`}></span>
           <span className={`${styles.bar}`}></span>
+          {isOpen && <span className={`${styles.close}`} onClick={handleToggle}>X</span>}
         </button>
         <ul className={`${styles.list} ${isOpen ? styles.open : ''}`}>
-          <li>Home</li>
-          <li>Posts</li>
-          <li>Sobre</li>
-          <li>Contato</li>
+
+          <Link className={styles.menuBlink} href={"/"}><li onClick={handleLinkClick}>Home</li></Link>
+          <Link className={styles.menuBlink} href={"/sobre"}><li onClick={handleLinkClick}>Sobre</li></Link>
+          <Link className={styles.menuBlink} href={"/contato"}><li onClick={handleLinkClick}>Contato</li></Link>
         </ul>
       </nav>
     </header>
   )
 }
 
-export default Header
+export default Header;
