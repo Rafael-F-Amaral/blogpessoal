99084bd81ee81bd720847bb628fc3c8fe3718ae0
Rafael Amaral <rafaelbmo@live.com>
2023-03-07 12:34:10 -0300

    85

--- next.config.js
+++ next.config.js
@@ -6,14 +6,14 @@ const nextConfig = {
 Este código desativa o fallback do módulo fs no client-side do Next.js
 para evitar erros no uso do pacote @sendgrid/mail.
 */
-// module.exports = {
-// webpack: (config, { isServer }) => {
-// if (!isServer) {
-// config.resolve.fallback.fs = false;
-// }
-
-// return config;
-// }
-// };
+//module.exports = {
+  //webpack: (config, { isServer }) => {
+   // if (!isServer) {
+  //    config.resolve.fallback.fs = false;
+  //  }
+//
+ //   return config;
+ // }
+//};
 
 
