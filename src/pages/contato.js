4dd629cad99a0042e058fa4c62119e474504cd94
Rafael Amaral <59116502+Rafael-F-Amaral@users.noreply.github.com>
2023-03-09 21:22:21 -0300

    1

--- src/pages/contato.js
+++ src/pages/contato.js
@@ -1,11 +1,35 @@
-import { FormEvent, useState } from 'react';
+import { useState } from 'react';
 import styles from '../pages/contato/contato.module.css';
 
+
 const Contato = () => {
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [mensagem, setMensagem] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    db.collection('mensagens').add({
+      nome,
+      email,
+      mensagem
+    })
+    .then(function(docRef) {
+      console.log("Mensagem enviada com sucesso!");
+      setNome('');
+      setEmail('');
+      setMensagem('');
+    })
+    .catch(function(error) {
+      console.error("Erro ao enviar mensagem: ", error);
+    });
+  }
+
   return (
     <div className={styles.contatoContainer}>
       <h2 className={styles.contatoTitle}>Entre em contato</h2>
-      <form className={styles.contatoForm}>
+      <form className={styles.contatoForm} onSubmit={handleSubmit}>
         <label className={styles.label} htmlFor="name">
           Nome:
         </label>
@@ -13,7 +37,8 @@ const Contato = () => {
           type="text"
           id="name"
           name="name"
-          
+          value={nome}
+          onChange={(event) => setNome(event.target.value)}
         />
 
         <label className={styles.label} htmlFor="email">
@@ -23,6 +48,8 @@ const Contato = () => {
           type="email"
           id="email"
           name="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
         />
 
         <label className={styles.label} htmlFor="message">
@@ -31,6 +58,8 @@ const Contato = () => {
         <textarea
           id="message"
           name="message"
+          value={mensagem}
+          onChange={(event) => setMensagem(event.target.value)}
         ></textarea>
 
         <button type="submit">Enviar</button>
